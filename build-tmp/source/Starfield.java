98678b1518c0d703525dfd0ca1ed9478fabcd93a
OliverNoss <odnoss@sbcglobal.net>
2014-10-20 12:20:16 -0700

    Starfield with Oddball, no jumbo

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,10 +1,26 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle[] field = new Particle[200];
 int resetCounter=0;
 int oddCount=0;
 boolean oddUp=true;
 int timer=0;
 float fade=50;
-void setup()
+public void setup()
 {
 	size(500,550);
 	background(0);
@@ -20,7 +36,7 @@ void setup()
 	}
 // frameRate(1);
 }
-void draw()
+public void draw()
 {
 	noStroke();
 	fill(0,0,0,fade);
@@ -74,7 +90,7 @@ class NormalParticle implements Particle
 	double x,y,dx,dy,angle,speed;
 	int ringWidth,ringHeight;
 	boolean reset;
-	color rgb;
+	int rgb;
 	NormalParticle()
 	{
 		reset=false;
@@ -209,4 +225,13 @@ class OddballParticle implements Particle
 
 	}
 	
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
